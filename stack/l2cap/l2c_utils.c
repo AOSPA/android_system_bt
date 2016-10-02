AOSPA/android_system_bt

commit 2772d76990d8424ef3c629dba164fc7111b37db3

Bluetooth: Dynamic ACL buffer allocation based on ACL Priority
1. A2dp acl link priority is set to high once streaming starts
2. Allocation min 2 ACL bufferswq to high priority link

CRs-fixed: 647447
Change-Id: I8f7742ae193998643ff2a12bf87f9874844ee0f7

stack/l2cap/l2c_utils.c
@@ -2613,6 +2613,12 @@ BOOLEAN l2cu_set_acl_priority (BD_ADDR bd_addr, UINT8 priority, BOOLEAN reset_af
         return (FALSE);
     }
 
+    if (p_lcb->acl_priority != priority)
+    {
+        p_lcb->acl_priority = priority;
+        l2c_link_adjust_allocation();
+    }
+
     if (BTM_IS_BRCM_CONTROLLER())
     {
         /* Called from above L2CAP through API; send VSC if changed */

stack/l2cap/l2c_link.c
@@ -85,6 +85,7 @@ BOOLEAN hci_blacklistted_for_role_switch (BD_ADDR addr)
     return FALSE;
 }
 
+#define HI_PRI_LINK_QUOTA 2 //Mininum ACL buffer quota for high priority link
 /*******************************************************************************
 **
 ** Function         l2c_link_hci_conn_req
@@ -898,6 +899,14 @@ void l2c_link_adjust_allocation (void)
     while ( (num_hipri_links * high_pri_link_quota + low_quota) > controller_xmit_quota )
         high_pri_link_quota--;
 
+    /*Adjust high pri link with min 3 buffers*/
+    if(num_hipri_links > 0)
+    {
+      if(high_pri_link_quota < HI_PRI_LINK_QUOTA)
+      {
+        high_pri_link_quota  = HI_PRI_LINK_QUOTA;
+      }
+    }
     /* Work out the xmit quota and buffer quota high and low priorities */
     hi_quota  = num_hipri_links * high_pri_link_quota;
     low_quota = (hi_quota < controller_xmit_quota) ? controller_xmit_quota - hi_quota : 1;

commit 68e347d848ce0108b7ead509d74e0f1c3d97a6f8

bluetooth: don't adjust ACL priority on Broadcom chipsets
commit 2772d76 caused
a regression in A2DP for Broadcom Bluetooth chipsets
by not sending the HCI_BRCM_SET_ACL_PRIORITY command.

Change this logic so the ACL priority adjustment occurs
unconditionally for non-Broadcom chipsets only.

Reproduction steps: Entering Bluetooth settings which
triggers inquiry and makes the device discoverable while
simultaneously using 2.4GHz WiFi.

Change-Id: Ic40dc8d66ae30c71e0ddf1a2eaeb19fee1e87ecb

stack/l2cap/l2c_utils.c
@@ -2572,12 +2572,6 @@ BOOLEAN l2cu_set_acl_priority (BD_ADDR bd_addr, UINT8 priority, BOOLEAN reset_af
         return (FALSE);
     }
 
-    if (p_lcb->acl_priority != priority)
-    {
-        p_lcb->acl_priority = priority;
-        l2c_link_adjust_allocation();
-    }
-
     if (BTM_IS_BRCM_CONTROLLER())
     {
         /* Called from above L2CAP through API; send VSC if changed */
@@ -2601,7 +2595,14 @@ BOOLEAN l2cu_set_acl_priority (BD_ADDR bd_addr, UINT8 priority, BOOLEAN reset_af
                 l2c_link_adjust_allocation();
             }
         }
+    } else {
+        if (p_lcb->acl_priority != priority)
+        {
+            p_lcb->acl_priority = priority;
+            l2c_link_adjust_allocation();
+        }
     }
+
     return(TRUE);
 }
 